0741af30214cd2375e9055819e59b7222242817e
jessec <robertjessecooke@gmail.com>
2014-10-22 12:14:32 +0200

    cc

--- slider/steps/step-1.js
+++ slider/steps/step-1.js
@@ -1,146 +1,22 @@
+
+
+
+var config = {
+		data : 'site/blocks/block-video/slider/data/slider.json',
+		schema : {
+			type : "object",
+			title : "Slider",
+			"properties" : {
+				"url1" : { "type" : "string" , "format": "url"},
+				"url2" : { "type" : "string" , "format": "url"},
+				"url3" : { "type" : "string" , "format": "url"},
+				"url4" : { "type" : "string" , "format": "url"}
+			}
+		}
+}
+
+
 function init(step) {
 
-	var parser = document.createElement('a');
-	parser.href = Wizard.config.pageUrl;
-	console.log(parser);
-
-	var dataRequest = Wizard.config.baseUrl + 'site/data.json?page='
-			+ parser.pathname + '&' + location.hash.substring(1);// 'edit-block-0-type-icon';
-
-	promise.get(dataRequest).then(function(error, text, xhr) {
-		if (error) {
-//			/alert('Error ' + xhr.status);
-			//return;
-			dataRequest = "";
-		}else{
-			console.log("result block data json : " + text);
-			var json = JSON.parse(text);
-		}
-		getData(dataRequest);
-	});
-
-	console.log(Wizard.config.pageUrl + location.hash);
-	function getData(dataRequest) {
-
-		var req = dataRequest;
-		if(req == ""){
-			req = Wizard.config.baseUrl + 'site/blocks/block-video/slider/data/photo.json';
-		}
-		promise
-				.get(req)
-				.then(
-						function(error, text, xhr) {
-							if (error) {
-								alert('Error ' + xhr.status);
-								return;
-							}
-							
-							var starting_value = "";
-							try{
-								starting_value = JSON.parse(text).data;
-							}catch(e){}
-							
-
-							if(typeof(starting_value) == "undefined" || starting_value == ""){
-								starting_value = JSON.parse(text);
-							}
-							// Initialize the editor
-							var editor = new JSONEditor(document
-									.getElementById('editor_holder-' + step), {
-								// Enable fetching schemas via ajax
-								ajax : true,
-								// The schema for the editor
-								schema : {
-									type : "object",
-									title : "Slider",
-									"properties" : {
-										"url1" : {
-											"url1" : "url"
-										},
-										"url2" : {
-											"url2" : "url"
-										},
-										"url3" : {
-											"url3" : "url"
-										},
-										"url4" : {
-											"url4" : "url"
-										}
-									}
-								},
-								// Seed the form with a starting value
-								startval : starting_value,
-								// Disable additional properties
-								no_additional_properties : true,
-								disable_edit_json : true,
-								disable_properties : true,
-								disable_collapse : true,
-								// Require all properties by default
-								required_by_default : true
-							});
-							// Hook up the submit button to log to the console
-							document
-									.getElementById('submit-' + step)
-									.addEventListener(
-											'click',
-											function(event) {
-												event.stopPropagation();
-												console.log(editor.validate());
-												Wizard.goToNextStep(event);
-												console
-														.log('sending to swagger api');
-												console.log(editor.getValue());
-
-												var hash = location.hash
-														.split('=');
-												var data = hash[1].split('-');
-												var meta = {
-													"absolute-url" : document
-															.getElementById(
-																	'iframe')
-															.getAttribute('src'),
-													"state" : data[0],
-													"block" : data[2],
-													"type" : data[4],
-													"template" : Wizard.widgetJson[data[4]].template
-												}
-												console.log(meta);
-
-												var fullData = {
-													"meta" : meta,
-													"editor" : editor
-															.getValue(),
-												}
-
-												var jsonString = JSON
-														.stringify(fullData);
-
-												var data = {
-													"id" : 112,
-													"data" : jsonString
-												};
-
-												promise
-														.post(
-																location.origin +'/api/block',
-																data)
-														.then(
-																function(error,
-																		text,
-																		xhr) {
-																	if (error) {
-																		alert('Error '
-																				+ xhr.status);
-																		return;
-																	}
-																	console
-																			.log(text);
-																	document
-																			.getElementById('iframe').contentWindow.location = location.href;
-																});
-
-											});
-
-						});
-	}
+	Wizard.run(step, config);
 }
