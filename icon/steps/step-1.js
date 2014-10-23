64b8896effd30430a5294fbff352f208de50f19a
Ezra Verheijen <e.verheijen@hotmail.com>
2014-10-23 15:52:44 +0200

    Update step-1.js

--- icon/steps/step-1.js
+++ icon/steps/step-1.js
@@ -1,47 +1,68 @@
 var config = {
-		data : 'site/blocks/block-video/icon/data/icon.json',
-		schema : {
-			type : "object",
-			title : "Icon",
-			"properties" : {
-				"title" : {
-					"type" : "string",
-					"default" : "",
-				},
-				"content" : {
-					"type" : "string",
-					"format" : "html",
-					 "options": {
-						"wysiwyg": true
-					}
-				},
-				"icon" : {
-					"type" : "string",
-					"enum" : [ "building", "eastholland", "flag", "globe", 
-						   "hands", "innovationprojects",
-						   "innovationvouchers", "knowlegdevouchers",
-						   "lamp", "lamps", "milestone", "money", "patent",
-						   "people", "roadmap", "rocket", "scale", "sign", 
-						   "speechballoon", "suitcase", "toparrangement"
-						],
-					"default" : "rocket",
-				},
-				"category" : {
-					"type" : "string",
-					"enum" : [ "cat-one", "cat-two", "cat-three",
-							"cat-four", "cat-five" ],
-					"default" : "cat-one",
-				},
-				"num" : {
-					"type" : "number",
-					"default" : 0,
-				},
-				"txt" : {
-					"type" : "string",
-					"default" : "",
-				}
-			}
-		}
+    "data": "site/blocks/block-video/icon/data/icon.json",
+    "schema": {
+        "type": "object",
+        "title": "Icon",
+        "properties": {
+            "title": {
+                "type": "string",
+                "default": ""
+            },
+            "content": {
+                "type": "string",
+                "format": "html",
+                "options": {
+                    "wysiwyg": true
+                }
+            },
+            "icon": {
+                "type": "string",
+                "enum": [
+                    "building",
+                    "eastholland",
+                    "flag",
+                    "globe",
+                    "hands",
+                    "innovationprojects",
+                    "innovationvouchers",
+                    "knowlegdevouchers",
+                    "lamp",
+                    "lamps",
+                    "milestone",
+                    "money",
+                    "patent",
+                    "people",
+                    "roadmap",
+                    "rocket",
+                    "scale",
+                    "sign",
+                    "speechballoon",
+                    "suitcase",
+                    "toparrangement"
+                ],
+                "default": "rocket"
+            },
+            "category": {
+                "type": "string",
+                "enum": [
+                    "cat-one",
+                    "cat-two",
+                    "cat-three",
+                    "cat-four",
+                    "cat-five"
+                ],
+                "default": "cat-one"
+            },
+            "num": {
+                "type": "number",
+                "default": 0
+            },
+            "txt": {
+                "type": "string",
+                "default": ""
+            }
+        }
+    }
 }
 
 
