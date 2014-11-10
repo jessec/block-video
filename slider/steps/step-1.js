50dc793c26a94f9dfb60ce86cab0180b8c355a11
jessec <robertjessecooke@gmail.com>
2014-11-10 11:21:04 +0100

    cc

--- slider/steps/step-1.js
+++ slider/steps/step-1.js
@@ -20,12 +20,26 @@ function init(step) {
 
 	callback = function(){
 
+		var name;
 
 		$('input[type=url]').on('click', function(){
 			console.log(this);
+			name = $(this).attr("name");
 			Core9.parent.send({
 				geturlservice : true
 			});
+
+			Core9.parent.receive(function(data, event) {
+				console.log('Callback recieved data message : ');
+				console.log(data);
+				console.log('Callback recieved url data : ');
+				console.log(data.state.url);
+				console.log("name : " + name);
+				$("input[name='"+name+"']").val(data.state.url);
+				console.log('Callback recieved event message : ');
+				console.log(event);
+			});
+
 		});
 
 	}
