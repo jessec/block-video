46bda429ebe494afe028b867303ad5bd8346b9c7
jessec <robertjessecooke@gmail.com>
2014-11-14 12:43:43 +0100

    cc

---
+++ anythingslider/steps/step-1.js
@@ -0,0 +1,53 @@
+var test = {};
+
+
+var config = {
+		data : 'site/blocks/block-video/anythingslider/data/anythingslider.json',
+		schema : {
+			type : "object",
+			title : "anythingslider",
+			"properties" : {
+				"url1" : { "type" : "string" , "format": "url"},
+				"url2" : { "type" : "string" , "format": "url"},
+				"url3" : { "type" : "string" , "format": "url"},
+				"url4" : { "type" : "string" , "format": "url"}
+			}
+		}
+}
+
+
+function init(step) {
+
+	callback = function(editor){
+
+
+
+		$('input[type=url]').on('dblclick', function(){
+			console.log(this);
+			test.name = $(this).attr("name");
+			Core9.parent.send({
+				geturlservice : true
+			});
+
+			Core9.parent.receive(function(data, event) {
+				console.log('Callback recieved data message : ');
+				console.log(data.state.url);
+
+				var tmpName = test.name.replace('root[','');
+				tmpName = tmpName.substring(0, tmpName.length - 1);
+
+				var name = editor.getEditor('root.'+tmpName);
+				if(name) {
+				  name.setValue(data.state.url);
+				  console.log(name.getValue());
+				}
+
+			});
+
+		});
+
+	}
+
+
+	Wizard.run(step, config, callback);
+}
