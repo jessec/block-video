cc633716b763fbd49405130c03ec9f745718ea73
jessec <robertjessecooke@gmail.com>
2014-11-27 21:37:29 +0100

    cc

---
+++ menus/steps/step-1.js
@@ -0,0 +1,41 @@
+var config = {
+		data : 'site/blocks/block-video/text/data/text.json',
+		schema : {
+			"title": "menus",
+            "type": "object",
+            "properties": {
+                "menus": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "heading": {
+                                "type": "string"
+                            },
+                            "items": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "properties": {
+                                        "link": {
+                                            "type": "string"
+                                        },
+                                        "title": {
+                                            "type": "string"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+		}
+}
+
+
+function init(step) {
+
+	Wizard.run(step, config);
+}
+
