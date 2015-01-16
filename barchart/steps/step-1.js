3367694e73c427680f2f0c414f2870bb871329e7
Ezra Verheijen <e.verheijen@hotmail.com>
2015-01-16 14:00:42 +0100

    Update step-1.js

--- barchart/steps/step-1.js
+++ barchart/steps/step-1.js
@@ -1,62 +1,69 @@
+
+
+
 var config = {
-  data: 'site/blocks/block-video/barchart/data/barchart.json',
-  schema: {
-    type: "object",
-    title: "Barchart",
-    "properties": {
-      "bars": {
-        "type": "array",
-        "items": {
-          "type": "object",
-          "properties": {
-            "name": {
-              "type": "string",
-              "default": "tw"
-            },
-            "values": {
-              "type": "string",
-              "default": "10,20,30"
-            },
-          },
-        },
-      },
-      "id": {
-        "type": "string"
-      },
-      "labels": {
-        "type": "string",
-        "default": "2012,2013,2014"
-      },
-      "title": {
-        "type": "string",
-        "default": ""
-      },
-      "popup_title": {
-        "type": "string",
-        "default": ""
-      },
-      "category": {
-        "type": "string",
-        "enum": [
-          "cat-one",
-          "cat-two",
-          "cat-three",
-          "cat-four",
-          "cat-five"
-        ],
-        "default": "cat-one"
-      },
-      "content": {
-        "type": "string",
-        "format": "html",
-        "options": {
-          "wysiwyg": true
-        }
-      }
-    }
-  }
+		data : 'site/blocks/block-video/barchart/data/barchart.json',
+		schema : {
+			type : "object",
+			title : "Barchart",
+			  "properties": {
+			  	"bars": {
+			  		"type": "array",
+			  		"items": {
+				  		"type": "object",
+				  		"properties": {
+				  			"name": {
+				  				"type": "string",
+				  				"default": "tw"
+				  			},
+				  			"values": {
+				  				"type": "string",
+				  				"default": "10,20,30"
+				  			},
+				  		},
+				  	},
+			  	},
+			  	"id": {
+	  				"type": "string"
+			  	},
+			  	"labels": {
+	  				"type": "string",
+	  				"default": "2012,2013,2014"
+			  	},
+			    "title": {
+			      "type": "string",
+			      "default": ""
+			    },
+			    "popup_title": {
+			      "type": "string",
+			      "default": ""
+			    },
+			    "category": {
+					"type": "string",
+					"enum": [
+					"cat-one",
+					"cat-two",
+					"cat-three",
+					"cat-four",
+					"cat-five"
+					],
+					"default": "cat-one"
+				},
+			    "content": {
+					"type": "string",
+					"format": "html",
+					"options": {
+						"wysiwyg": true
+					},
+				},
+			  },
+		},
 }
 
+
 function init(step) {
-  Wizard.run(step, config);
+
+	Wizard.run(step, config);
 }
+
+
